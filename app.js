27cdcd56e86fb67cc894c80619870671ffc942c5
rifqirizairfansyah <38659907+rifqirizairfansyah@users.noreply.github.com>
2022-06-30 23:53:15 +0700

    cloudinary upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express()
-// const upload = require('./multer')
+const upload = require('./multer')
 const cloudinary = require('./cloudinary')
 const fs = require('fs');
 
@@ -10,37 +10,26 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
-const multer = require('multer');
+app.use(
+  '/create',
+  async (req, res, next) => {
+      upload.array(
+        'image'
+      )(req, res, (err) => {
+          if (err) {
+                  // SEND CAUGHT ERROR!
+                  res.send(err)
+              }
+              else { next() }
+          })
+      },
+      async (req, res) => {
+          // do stuff here
+      }
+)
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/')
-  }
-})
-
-
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === "video/mp4") {
-    cb(null, true)
-  } else {
-    //reject file
-    console.log("tidak diterima");
-    req.fileValidationError = 'goes wrong on the mimetype';
-    return cb(new Error('goes wrong on the mimetype'), false);
-  }
-}
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 2048 * 2048
-  },
-  fileFilter: fileFilter
-})
-
-app.use('/upload-images', async (req, res) => {
+app.use('/upload-images',upload.array('image'), async (req, res) => {
   
-  const uploads = upload.array('image')
   uploads(req, res, (err) => {
     console.log("masuk")
     if(err) {
@@ -51,17 +40,17 @@ app.use('/upload-images', async (req, res) => {
     res.send(req.file);
   });
 
-  // const uploader = async (path) => await cloudinary.uploads(path, 'DEV');
+  const uploader = async (path) => await cloudinary.uploads(path, 'DEV');
 
   if (req.method === 'POST') {
-  //   const urls = []
-  //   const files = req.files;
-  //   for (const file of files) {
-  //     const { path } = file;
-  //     const newPath = await uploader(path)
-  //     urls.push(newPath)
-  //     fs.unlinkSync(path)
-  //   }
+    const urls = []
+    const files = req.files;
+    for (const file of files) {
+      const { path } = file;
+      const newPath = await uploader(path)
+      urls.push(newPath)
+      fs.unlinkSync(path)
+    }
 
     res.status(200).json({
       message: 'images uploaded successfully',
--- index.js
+++ index.js
@@ -1,6 +1,60 @@
-const http = require('http');
-const app = require('./app');
-const port = process.env.PORT || 5002;
-const server = http.createServer(app);
+const express = require('express')
+const multer = require('multer');
 
-server.listen(port);
+const app = express();
+const cloudinary = require('./cloudinary')
+const fs = require('fs');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 2048 * 2048 * 5
+    },
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype === "video/mp4" || file.mimetype === "application/pdf" || file.mimetype === "application/msword") {
+            cb(null, true);
+        } else {
+            return cb(new Error('Invalid mime type'));
+        }
+    }
+});
+
+const uploadSingleImage = upload.single('file');
+
+app.post('/upload', async function (req, res) {
+
+    uploadSingleImage(req, res, async function (err) {
+
+        if (err) {
+            return res.status(400).send({ message: err.message })
+        }
+
+        // Everything went fine.
+        const file = req.file;
+        
+        const uploader = async (path) => await cloudinary.uploads(path, 'DEV');
+        const onlinePath = await uploader(file.path)
+        fs.unlinkSync(file.path)
+
+        res.status(200).send({
+            online_path: onlinePath, 
+            path: file.path,
+            filename: file.filename,
+            mimetype: file.mimetype,
+            originalname: file.originalname,
+            size: file.size,
+            fieldname: file.fieldname
+        })
+    })
+})
+
+app.listen(5002, () => {
+    console.log("Server is listening on port: 5002");
+});
